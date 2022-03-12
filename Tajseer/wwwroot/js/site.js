3d62348085ee61c944476d19b87cd042df664165
Suhib albayary <salbyari3@gmail.com>
2022-03-12 16:59:30 +0300

    edit

--- Tajseer/wwwroot/js/site.js
+++ Tajseer/wwwroot/js/site.js
@@ -34,3 +34,12 @@ $(function () {
         cursorwidth: "10px" }
           );
 });
+
+// Mobile Navigation Menu
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
